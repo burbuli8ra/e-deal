74ad366ccd2e7b31f62b66cb7518dd32fea7517a
Marilena Ioannou <marilena.ioannou.sg@gmail.com>
2020-11-08 09:16:04 +0200

    Implementation of API calls

---
+++ src/api/index.js
@@ -0,0 +1,51 @@
+import qs from 'qs';
+
+const API = 'https://bp-interview.herokuapp.com';
+
+const appendSearchParams = (url, searchParams) => {
+  if (Object.keys(searchParams).length > 0) {
+    return `${url}?${qs.stringify(searchParams)}`
+  }
+
+  return url;
+};
+
+const apiRequest = ({
+  url,
+  searchParams = {}
+}) => {
+  const headers = new Headers({
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  });
+
+  const request = new Request(appendSearchParams(url, searchParams), {
+    headers,
+    method: 'GET'
+  });
+
+  return fetch(request)
+    .then(response => response.json())
+    .catch(error => error);
+};
+
+const fetchRoute = {
+  category: categoryId =>
+    apiRequest({
+      url: `${API}/categories/${categoryId}`
+    }),
+  categoriesList: () =>
+    apiRequest({
+      url: `${API}/categories`
+    }),
+  product: productId =>
+    apiRequest({
+      url: `${API}/products/${productId}`
+    }),
+  productsList: categoryId =>
+    apiRequest({
+      url: `${API}/categories/${categoryId}/products`
+    })
+};
+
+export default fetchRoute;
